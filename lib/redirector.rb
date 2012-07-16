e90f9f19ed5f267310cfaf8d058b5ac81c3b8f4b
Gustav Jonsson <gustav@invoke.se>
2012-07-16 18:08:19 +0200

    Add tracking_url option

--- lib/redirector.rb
+++ lib/redirector.rb
@@ -1,36 +1,44 @@
 module Redirector
-  
+
   require "uri"
   require 'redirector/engine'
   require 'redirector/redirect_path'
   require 'redirector/redirectable'
   require 'redirector/models'
-  
+
   if defined?(::Mongoid::Document)
     Mongoid::Document::ClassMethods.class_eval do
       include Redirector::Models
     end
   end
-  
+
   ActiveRecord::Base.extend(Redirector::Models) if defined?(::ActiveRecord::Base)
-      
+
   mattr_accessor :epi_pattern
   @@epi_pattern = /\{epi\}/
-  
+
   mattr_accessor :url_pattern
   @@url_pattern = /\{url\}/
-  
-  # Affiliate networks that doesn't support encoded urls for landing-pages 
+
+  mattr_accessor :tracking_url
+  @@tracking_url = nil
+
+  # Affiliate networks that doesn't support encoded urls for landing-pages
   mattr_accessor :ignore_encoding_hosts
-  @@ignore_encoding_hosts = ["affiliator.com","adtraction.com","partner-ads.com","smartresponse-media.com"]
-  
+  @@ignore_encoding_hosts = [
+    "affiliator.com",
+    "adtraction.com",
+    "partner-ads.com",
+    "smartresponse-media.com"
+  ]
+
   mattr_accessor :redirect_link_options
   @@redirect_link_options = { 'data-external' => 'true', 'rel' => 'nofollow' }
-  
+
   def self.setup
     yield self
   end
-  
+
 end
 
 
