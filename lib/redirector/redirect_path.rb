867311f5d2d2b883595a55767033ec6a57b0fadf
Gustav Jonsson <gustav@invoke.se>
2012-07-16 18:07:58 +0200

    Extract host from redirect_path

--- lib/redirector/redirect_path.rb
+++ lib/redirector/redirect_path.rb
@@ -3,29 +3,33 @@ require "cgi"
 module Redirector
 
   class RedirectPath
-    
+
     attr_accessor :base, :landing_page, :epi
-    
+
     def build_url
       return nil if landing_page.blank?
       redirect_url = base.present? ? base_with_landing_page : landing_page
-      redirect_url.gsub(Redirector::epi_pattern,epi.to_s.downcase)
+      redirect_url.gsub(Redirector::epi_pattern, epi.to_s)
     end
-  
+
     protected
-  
+
       def base_with_landing_page
-        self.base.gsub(Redirector::url_pattern,try_escape_url)
+        base.gsub(Redirector::url_pattern, try_escape_url)
       end
-  
+
+      def host
+        @host ||= URI.parse(base).host
+      end
+
+      def ignore_encoding?
+        Redirector::ignore_encoding_hosts.include?(host)
+      end
+
       def try_escape_url
-        if Redirector::ignore_encoding_hosts.any? { |x| self.base.include?(x) }
-          landing_page
-        else
-          CGI.escape(landing_page)
-        end
+        ignore_encoding? ? landing_page : CGI.escape(landing_page)
       end
-  
+
   end
 
 end
