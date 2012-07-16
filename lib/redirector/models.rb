0e9037544d6b26e1220eb9bf92c43234531460cb
Gustav Jonsson <gustav@invoke.se>
2012-07-16 18:06:20 +0200

    Fix whitespace

--- lib/redirector/models.rb
+++ lib/redirector/models.rb
@@ -1,15 +1,15 @@
 module Redirector
   module Models
-    
+
     # Defines the methods to call when generating the redirect path
     # redirect_with :base => proc { |x| x.store.affiliate_uri }, :path => :url, :epi => proc { |x| "foo_#{x.id}"}
     #
-    def redirect_with(*args)     
+    def redirect_with(*args)
       include Redirector::Redirectable
       options = args.extract_options!
-      options.assert_valid_keys(:base,:path,:epi,:find_method)
+      options.assert_valid_keys(:base, :path, :epi, :find_method)
       self.redirect_attributes_options = options.reverse_merge(:find_method => :find)
     end
 
-  end    
+  end
 end
