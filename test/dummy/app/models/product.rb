ffe110df962303a94863db7bd1e4a909f28d3652
Gustav Jonsson <gustav@invoke.se>
2012-10-01 16:24:05 +0100

    Change dummy app

--- test/dummy/app/models/product.rb
+++ test/dummy/app/models/product.rb
@@ -1,15 +1,15 @@
 class Product
-  
+
   include Mongoid::Document
-  
+
   field :name
   field :url
   field :affiliate_uri
-    
+
   redirect_with :base => proc { |x| x.affiliate_uri }, :path => :url, :epi => :epi
-      
+
   def epi
-    "product_#{name}"
+    "product_#{name}".parameterize
   end
 
   def to_param
