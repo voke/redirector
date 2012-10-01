5441e83092ebd436bd3f35630f03cd0c4da52486
Gustav Jonsson <gustav@invoke.se>
2012-10-01 16:23:10 +0100

    Refactor RedirectPath methods

--- lib/redirector/redirect_path.rb
+++ lib/redirector/redirect_path.rb
@@ -7,34 +7,45 @@ module Redirector
 
     attr_accessor :base, :landing_page, :epi
 
+    def initialize(attrs = {})
+      attrs.each do |key, value|
+        send("#{key}=", value)
+      end
+    end
+
     def build_url
       return nil if landing_page.blank?
       redirect_url = base.present? ? base_with_landing_page : landing_page
-      redirect_url.gsub(Redirector::epi_pattern, epi.to_s)
+      if Redirector::epi_pattern
+        redirect_url.gsub(Redirector::epi_pattern, epi.to_s)
+      else
+        redirect_url
+      end
     end
 
-    protected
+    def base_with_landing_page
+      base.gsub(Redirector::url_pattern, try_escape_url)
+    end
 
-      def base_with_landing_page
-        base.gsub(Redirector::url_pattern, try_escape_url)
-      end
+    def host
       @host ||= Addressable::URI.parse(base).host
+    end
 
-      def host
-        @host ||= URI.parse(base).host
-      end
     def parse_to_regex(str)
       escaped = Regexp.escape(str).gsub('\*','.*?')
       Regexp.new "^#{escaped}$", Regexp::IGNORECASE
     end
 
-      def ignore_encoding?
-        Redirector::ignore_encoding_hosts.include?(host)
+    def ignore_encoding?
+      Redirector::ignore_encoding_hosts.any? do |rule|
+        regexp = parse_to_regex(rule)
+        host.match(regexp)
       end
+    end
 
-      def try_escape_url
-        ignore_encoding? ? landing_page : CGI.escape(landing_page)
-      end
+    def try_escape_url
+      ignore_encoding? ? landing_page : CGI::escape(landing_page)
+    end
 
   end
 
