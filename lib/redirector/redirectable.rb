a5911dbcb1d73cf3bbb39103ed708bea8dd524da
Gustav Jonsson <gustav@invoke.se>
2012-10-01 16:23:33 +0100

    Try add redirect_tracking url if exists

--- lib/redirector/redirectable.rb
+++ lib/redirector/redirectable.rb
@@ -2,14 +2,14 @@ module Redirector
   module Redirectable
 
     extend ActiveSupport::Concern
-        
+
     included do
       class_attribute :redirect_attributes_options, :instance_writer => false
       self.redirect_attributes_options = {}
     end
-    
+
     module ClassMethods
-      
+
       # This is how the resource is loaded.
       #
       # You might want to overwrite this method when you are using custom finder method.
@@ -22,28 +22,37 @@ module Redirector
       def find_for_redirect(resource_id)
         send(redirect_attributes_options[:find_method], resource_id)
       end
-      
+
     end
-    
+
     # Returns the generated url depending on redirect_options.
     # This is the method that is called on the resource in the controller.
-    #        
+    #
     def redirect_path
       generate_redirect_url
     end
-                  
+
     private
-      
+
       # Returns the complete url depending on redirect_options.
-      #      
-      def generate_redirect_url        
+      #
+      def generate_redirect_url
         rp = RedirectPath.new
         rp.landing_page = call_redirect_option(:path)
         rp.base = call_redirect_option(:base)
         rp.epi = call_redirect_option(:epi)
-        rp.build_url
+        affiliate_url = rp.build_url
+        try_add_redirect_tracking_url(affiliate_url)
       end
-        
+
+      def try_add_redirect_tracking_url(url)
+        if Redirector::tracking_url
+          Redirector::tracking_url.gsub(Redirector::url_pattern, url)
+        else
+          url
+        end
+      end
+
       # Make different call depending on option.
       #
       def call_redirect_option(key)
@@ -56,6 +65,6 @@ module Redirector
             callback
         end
       end
-          
+
   end
 end
