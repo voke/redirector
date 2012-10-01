30fc3c1f24d7ef31ddbff7197483e48f4209efc5
Gustav Jonsson <gustav@invoke.se>
2012-10-01 16:25:00 +0100

    Update gems

--- lib/redirector/engine.rb
+++ lib/redirector/engine.rb
@@ -3,13 +3,13 @@ require 'redirector/rails/view_helpers'
 
 module Redirector
   class Engine < Rails::Engine
-      
+
       # TODO : I can't find a better way to include ViewHelpers by default.
       initializer "redirector.view_helpers" do
         config.after_initialize do
           ActionView::Base.send :include, Redirector::ViewHelpers
         end
       end
-      
+
   end
 end
