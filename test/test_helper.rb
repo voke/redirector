d627735fe338111324b541ee9407a1d368fac3e9
Gustav Jonsson <gustav@invoke.se>
2012-10-08 23:07:15 +0100

    Remove swap helper metod

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,18 +13,6 @@ Dir["#{File.dirname(__FILE__)}/fabricators/**/*.rb"].each { |f| require f }
 class ActiveSupport::TestCase
 
   # Helper methods to be used by all tests here...
-  def swap(object, new_values)
-    old_values = {}
-    new_values.each do |key, value|
-      old_values[key] = object.send key
-      object.send :"#{key}=", value
-    end
-    yield
-  ensure
-    old_values.each do |key, value|
-      object.send :"#{key}=", value
-    end
-  end
 
   def teardown
 
