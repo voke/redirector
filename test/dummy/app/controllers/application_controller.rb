8c137d4301f22213265f281761bb02db614be4a7
Gustav Jonsson <gustav@invoke.se>
2013-12-06 12:46:08 +0100

    Add before_filter to set locale in dummy app

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
 
   helper Redirector::Engine::helpers
 
-  def before_redirect(resource)
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale]
+  end
+
+  def before_redirect(resource, path)
+    # render text: @redirect_path
+    # return false
   end
 
 end
+
+
