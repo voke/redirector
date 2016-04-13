e4a31f3188fcf7f67166845d6e35ee040fa18b69
Gustav <gustav@invoke.se>
2016-04-13 11:55:18 +0200

    Add controller as option to redirect_for

--- lib/redirector/rails/routes.rb
+++ lib/redirector/rails/routes.rb
@@ -1,19 +1,19 @@
 module ActionDispatch::Routing
-  
+
   class Mapper
-        
+
     # Helper method to generate the named_route(s) needed for the resource(s).
-    # 
+    #
     #   redirect_for :products
     #
     # => redirect_product /products/:product_id/redirect(.:format) { :controller=>"redirector/redirect", :action=>"redirect" }
-    # 
-    def redirect_for(*resources)
+    #
+    def redirect_for(*resources, controller: 'redirector/redirect')
       resources.map(&:to_s).each do |resource|
-        get "#{resource}/:#{resource.singularize}_id/redirect" => "redirector/redirect#redirect", :as => "redirect_#{resource.singularize}"
+        get "#{resource}/:#{resource.singularize}_id/redirect" => "#{controller}#redirect", :as => "redirect_#{resource.singularize}"
       end
     end
-      
+
   end
-  
+
 end
